8692e7f7789285cf0bf19257813bf1333d6a755a
[SomezakiNayuki] <[2502651290@qq.com]>
2021-09-14 18:45:12 +1000

    Time changes to China and Sydney time, Due time added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hello-dizzy-bot-project
 A little hello program
+
+0.0.1:
+    Now it can only showing London time but could boot at 
+    any directory, to set up, use 'sudo cp hello-dizzy-bot /usr/local/bin'
+
+0.0.2:
+    Time changes to China and Sydney time, Due time added
--- hello-dizzy-bot.c
+++ hello-dizzy-bot.c
@@ -3,13 +3,48 @@
 #include <string.h>
 #include <time.h>
 
-int main(void) {
-    char *hello = {"Hi! I'm dizzy bot, your personal greating assistant\nI wish you happy every day :)\n"};
-    printf("%s", hello);
+void print_hello();
+void print_time();
 
-    time_t timep;
-    time(&timep);
-    printf("Now it is %s\n", asctime(gmtime(&timep)));
+int main(void) {
+    print_hello();
+    print_time();
 
     return 0;
+}
+
+void print_hello() {
+    char *print_hello = {"Hi! I'm dizzy bot\nyour personal greating assistant\nI wish you happy every day :)\n\n"};
+    printf("%s", print_hello);
+}
+
+void print_time() {
+    char *wday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+    time_t time_stamp;
+    struct tm *time_pointer;
+    int SYD_day_light_saving = 10;
+
+    time(&time_stamp);
+    time_pointer =  gmtime(&time_stamp);
+    
+    printf("Now it is:\n\t%d-%d-%d %s\n", 1900 + time_pointer->tm_year,
+                                          time_pointer->tm_mon,
+                                          time_pointer->tm_mday,
+                                          wday[time_pointer->tm_wday]);
+
+    if (time_pointer->tm_isdst) {
+        SYD_day_light_saving++;
+    }
+
+    printf("China:\n\t%d:%d:%d\n", 8 + time_pointer->tm_hour,
+                                   time_pointer->tm_min,
+                                   time_pointer->tm_sec);
+    printf("Sydney:\n\t%d:%d:%d\n", SYD_day_light_saving + time_pointer->tm_hour,
+                                    time_pointer->tm_min,
+                                    time_pointer->tm_sec);
+
+    printf("Due:\n\t%d days %d hours %d mins left\n", 7 - time_pointer->tm_wday,
+                                                      20 - (SYD_day_light_saving + time_pointer->tm_hour),
+                                                      60 - time_pointer->tm_min);
 }
